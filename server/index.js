ecb122a57f335cb6964a0ad0eb7cbb143558c1f0
Vivek Dhuri <vivek.dhuri1@t-mobile.com>
2021-11-23 23:21:03 -0500

    Changed listener port settings
    Changed listener port settings

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const port = 5000;
 const app = require('./app');
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
